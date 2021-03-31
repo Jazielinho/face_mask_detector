3c40349454ae61f08f8cec324848b4f3a4d34678
jazielinho <jahazielponce@gmail.com>
2021-03-31 11:11:26 +0200

    mask detector

--- config.py
+++
@@ -1,6 +0,0 @@
-'''
-Autor: Jazielinho
-'''
-
-color_dict = {1: (0, 255, 0), 0: (0, 0, 255)}
-labels_dict = {0: 'no mascara', 1: 'mascara'}
--- mask_detector_app.py
+++ mask_detector_app.py
@@ -8,6 +8,7 @@ import numpy as np
 import tensorflow as tf
 from training import config_tr
 import streamlit as st
+from PIL import Image
 import cv2
 from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
 
@@ -66,8 +67,8 @@ def get_predictions(face_to_predict: List) -> List:
 # ==================================================================================================================
 class VideoTransformer(VideoTransformerBase):
     # Clase para predecir máscaras de vídeos
-    def transform(self, frame):
-        img = frame.to_ndarray(format="bgr24")
+    @staticmethod
+    def transform_(img: np.array) -> np.array:
         face_to_predict, list_ubications = prepara_imagen_array(img=img)
         list_clases = get_predictions(face_to_predict=face_to_predict)
 
@@ -79,6 +80,11 @@ class VideoTransformer(VideoTransformerBase):
                 img = cv2.rectangle(img, (x, y - 40), (x+w, y), color_dict[list_clases[enum]], -2)
                 img = cv2.putText(img, labels_dict[list_clases[enum]], (x, y - 10), cv2.FONT_HERSHEY_COMPLEX, 0.75,
                                   (255, 255, 255), 1, cv2.LINE_AA)
+        return img, list_clases
+
+    def transform(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+        img, list_clases = VideoTransformer.transform_(img=img)
         return img
 
 
@@ -86,7 +92,20 @@ class VideoTransformer(VideoTransformerBase):
 st.title('Detección automática de máscaras')
 
 st.write("Esta aplicación identifica en tiempo real si tiene o no máscara.")
+st.write("Para más información puede ir al siguiente enlace: ")
+
 st.write("Para más información: ")
 
+status = st.sidebar.radio("Elija subir imagen o acceder a la camara web", ("Subir imagen", "Camara web"))
 
-webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
+if status == "Camara web":
+    webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
+else:
+    uploaded_file = st.file_uploader("Sube imagen", type=["png", "jpg", "jpeg"])
+    if uploaded_file is not None:
+        file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+        image = cv2.imdecode(file_bytes, 1)
+        new_image, list_clases = VideoTransformer.transform_(img=image)
+        text = f"Hay {len(list_clases)} encontradas, {len([x for x in list_clases if x > 0])} con máscara"
+        st.image(new_image, caption=text, use_column_width=True, channels="BGR")
+
