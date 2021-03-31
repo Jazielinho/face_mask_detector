5dec1af3208636cd8a175d31ee912b812d5b03bc
jazielinho <jahazielponce@gmail.com>
2021-03-31 10:23:12 +0200

    mask detection

--- face_recognition.py
+++ face_recognition.py
@@ -12,7 +12,7 @@ faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontal
 
 
 def detecta_rostros(frame: np.array) -> Tuple[List, List]:
-    ''' Retorna '''
+    ''' Retorna lista con imágenes de rostros y su ubicación en la imagen original '''
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = faceCascade.detectMultiScale(
